19c7c903fb2fec0cc4398f47381a23210a05834d
wuke1993 <2297184250@qq.com>
2017-06-11 10:00:05 +0800

    Add method storePreferenceMatrix.

--- src/org/recommender/preference/DLCPreferenceReader.java
+++ src/org/recommender/preference/DLCPreferenceReader.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 
+import org.recommender.utility.GetProperty;
+import org.recommender.utility.StoreStringIntoFile;
+
 /**
 * @author : wuke
 * @date   : 20170610 23:48:42
@@ -12,12 +15,25 @@ import java.io.FileReader;
 */
 public class DLCPreferenceReader implements PreferenceMatrixReader {
 
+	public static void main(String[] args) {
+		String preference_path = GetProperty.getPropertyByName("PREFERENCE_PATH");
+		int user_num = Integer.parseInt(GetProperty.getPropertyByName("USER_NUM"));
+		int item_num = Integer.parseInt(GetProperty.getPropertyByName("ITEM_NUM"));
+		
+		new DLCPreferenceReader().readPreferenceMatrix(preference_path, user_num, item_num);
+	}
 	@Override
 	public double[][] readPreferenceMatrix(String path, int user_num, int item_num) {
 		// user_num = Integer.parseInt(GetProperty.getPropertyByName("USER_NUM"));
 		// item_num = Integer.parseInt(GetProperty.getPropertyByName("ITEM_NUM_NUM"));
 		
 		double[][] preferenceMatrix = new double[user_num][item_num];
+		
+		for(int i = 0; i < preferenceMatrix.length; i++) {
+			for(int j = 0; j < (preferenceMatrix[i].length - 1); j++) {
+				preferenceMatrix[i][j] = 0.0;
+			}
+		}
 		
 		// read from file
 		int stuno_sequence = 0;
@@ -44,7 +60,31 @@ public class DLCPreferenceReader implements PreferenceMatrixReader {
 			e.printStackTrace();
 		}
 		
+		String path2 = GetProperty.getPropertyByName("PREFERENCE_MATRIX_PATH");
+		DLCPreferenceReader.storePreferenceMatrix(preferenceMatrix, path2);
+		
 		return preferenceMatrix;
 	}
 
+	/**
+	 * Store the preference matrix.
+	 * @param preferenceMatrix
+	 * @param path
+	 */
+	private static void storePreferenceMatrix(double[][] preferenceMatrix, String path) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < preferenceMatrix.length; i++) {
+			for(int j = 0; j < (preferenceMatrix[i].length - 1); j++) {
+				sb.append(preferenceMatrix[i][j]);
+				sb.append(",");
+			}
+			sb.append(preferenceMatrix[i][preferenceMatrix[i].length - 1]);
+			sb.append("\n");
+		}
+		
+		StoreStringIntoFile.storeString(sb.toString(), path);
+	}
+	
 }
