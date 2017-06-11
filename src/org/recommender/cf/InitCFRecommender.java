3bf0f6b5595bfbff5c4a4a901707994500ef2c9c
wuke1993 <2297184250@qq.com>
2017-06-12 05:55:47 +0800

    Add statements calling methods to generate recommendation and measuring.

--- src/org/recommender/cf/InitCFRecommender.java
+++ src/org/recommender/cf/InitCFRecommender.java
@@ -1,5 +1,10 @@
 package org.recommender.cf;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+
 import org.recommender.cf.neighborhood.CalNeighbors;
 import org.recommender.cf.preference.CalPreference;
 import org.recommender.cf.preference.DLCPreferenceReader;
@@ -7,6 +12,8 @@ import org.recommender.cf.preference.PreferenceDrag;
 import org.recommender.cf.preference.PreferenceDuration;
 import org.recommender.cf.preference.PreferenceTimes;
 import org.recommender.cf.similarity.CalSimilarityMatrix;
+import org.recommender.measuring.Measuring;
+import org.recommender.measuring.TestDataReader;
 import org.recommender.utility.GetProperty;
 
 /**
@@ -27,14 +34,14 @@ public class InitCFRecommender {
 		double coefficient_times = Double.parseDouble(GetProperty.getPropertyByName("WEIGHT_TIMES"));
 		double coefficient_drag = Double.parseDouble(GetProperty.getPropertyByName("WEIGHT_DRAG"));
 		double coefficient_duration = Double.parseDouble(GetProperty.getPropertyByName("WEIGHT_DURATION"));
-		
 		String path1 = GetProperty.getPropertyByName("PREFERENCE_TIMES_PATH");
 		String path2 = GetProperty.getPropertyByName("PREFERENCE_DRAG_PATH");
 		String path3 = GetProperty.getPropertyByName("PREFERENCE_DURATION_PATH");
 		String path4 = GetProperty.getPropertyByName("PREFERENCE_PATH");
 		
 		CalPreference calculator= new CalPreference();
-		calculator.calPreference(coefficient_times, coefficient_drag, coefficient_duration, path1, path2, path3, path4);*/
+		calculator.calPreference(coefficient_times, coefficient_drag, coefficient_duration, 
+				path1, path2, path3, path4);*/
 		
 		// calculate similarity, first read users' preference matrix
 		String preference_path = GetProperty.getPropertyByName("PREFERENCE_PATH");
@@ -50,10 +57,26 @@ public class InitCFRecommender {
 		int parameter_k = Integer.parseInt(GetProperty.getPropertyByName("PARAMETER_K"));
 		int[][] neighborsMatrix = CalNeighbors.calKNeighbors(user_num, parameter_k, similarityMatrix);
 		
-		// generate recommendations
+		// generate recommendations for one
+		int recommendation_num = 25;
+		/*int user_sequence = 1;
+		HashMap<Integer, Double> rec = GenRecommendations.genRecommendationForOne(preferenceMatrix, neighborsMatrix, user_sequence, recommendation_num);
 		
+		for(Entry<Integer, Double> entry : rec.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}*/
 		
-		// measuring
+		// generate recommendations for all
+		ArrayList<HashMap<Integer, Double>> recArr = GenRecommendations.genRecommendationForAll(preferenceMatrix, neighborsMatrix, recommendation_num);
+		
+		// store recommendations into file
+		String rec_path = GetProperty.getPropertyByName("REC_PATH");
+		GenRecommendations.storeRecommendations(recArr, rec_path);
+		
+		// measuring, first read test data
+		HashMap<Integer, HashSet<Integer>> stuno_videos = TestDataReader.readStunoVideos();
+		
+		Measuring.calAccuracyRate(recArr, stuno_videos);
 		
 	}
 }
