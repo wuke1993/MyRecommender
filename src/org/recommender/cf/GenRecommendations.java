fb1741e9a705bc510afb3e40b2a032ee41ead8c8
wuke1993 <2297184250@qq.com>
2017-06-11 11:21:08 +0800

    Generate recommendations.

---
+++ src/org/recommender/cf/GenRecommendations.java
@@ -0,0 +1,15 @@
+package org.recommender.cf;
+/**
+* @author : wuke
+* @date   : 20170611 10:53:54
+* Title   : GenRecommendations
+* Description : 
+*/
+public class GenRecommendations {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
