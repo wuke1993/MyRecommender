8ffe92675de22ae9c03a311a75eb0c9289bfefd5
wuke1993 <2297184250@qq.com>
2018-01-17 16:39:47 +0800

    bean，存储日志

---
+++ src/org/recommender/data/LearningLog.java
@@ -0,0 +1,37 @@
+package org.recommender.data;
+/**
+* @author : wuke
+* @date   : 20180117 15:12:14
+* Title   : LearningLog
+* Description : 
+*/
+public class LearningLog {
+	private long stuno;
+	private int oper;
+	private String title;
+	private int tlen;
+	
+	public long getStuno() {
+		return stuno;
+	}
+
+	public int getOper() {
+		return oper;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getTlen() {
+		return tlen;
+	}
+
+	public LearningLog(long stuno, int oper, String title, int tlen) {
+		super();
+		this.stuno = stuno;
+		this.oper = oper;
+		this.title = title;
+		this.tlen = tlen;
+	}
+}
