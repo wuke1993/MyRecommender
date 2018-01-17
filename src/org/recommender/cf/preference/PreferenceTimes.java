c85cd3d2d572ffc5c2ad9bdf17c1d871914bec16
wuke1993 <2297184250@qq.com>
2018-01-17 16:29:26 +0800

    76号操作，视频观看次数，评分构成之一。新增 calPreference() 重载方法，日志来源不同

--- src/org/recommender/cf/preference/PreferenceTimes.java
+++ src/org/recommender/cf/preference/PreferenceTimes.java
@@ -4,32 +4,31 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.recommender.utility.MySqlConn;
+import org.recommender.data.LearningLog;
+import org.recommender.utility.GetProperty;
+import org.recommender.utility.MySQLHelper;
 import org.recommender.utility.StoreStringIntoFile;
 
 /**
 * @author : wuke
 * @date   : 20170608 16:55:08
 * Title   : PreferenceTimes
-* Description : 
+* Description : (stuno, (video, times))
 */
 public class PreferenceTimes implements Preference {
-
 	public static void main(String[] args) {
 
-		Connection conn = MySqlConn.getConn();
+		Connection conn = MySQLHelper.getConn();
 		
-		String tableName = "my_maozedong_sequence";
-		String sql = "SELECT stuno, tlen, title, sequence FROM " + tableName 
-				+ " WHERE rtime BETWEEN \"2015-02-01 00:00:00\" AND \"2015-06-31 23:59:59\""; // We got 3766 students!
+		String tableName = "my_cs_log_stulearns_4th";
+		String sql = "SELECT stuno, title FROM " + tableName + " WHERE platform = 2 AND oper = 76";
 		
-		String path1 = "E:\\data\\DLC_forum\\recommender\\cf_preferenceTimes_detail.txt";
-		String path2 = "E:\\data\\DLC_forum\\recommender\\cf_preferenceTimes.txt";
-		
+		String path1 = GetProperty.getPropertyByName("PREFERENCE_TIMES_DETAIL_PATH");
+		String path2 = GetProperty.getPropertyByName("PREFERENCE_TIMES_PATH");
 		PreferenceTimes preferenceTimes = new PreferenceTimes();
 		preferenceTimes.calPreference(conn, sql, path1, path2);
 	}
@@ -37,60 +36,35 @@ public class PreferenceTimes implements Preference {
 	@Override
 	public void calPreference(Connection conn, String sql, String path1, String path2) {
 		Map<Long, HashMap<Integer, Integer>> stuno_video_times = new HashMap<Long, HashMap<Integer, Integer>>();
-		Map<Long, Integer> stuno_videos_times_sum = new HashMap<Long, Integer>();
-				
-		Map<Long, HashSet<String>> stuno_videos = new HashMap<Long, HashSet<String>>();
-        Map<Long, Integer> stuno_videos_num = new HashMap<Long, Integer>();
+		
+		HashMap<String, Integer> videos = VideoSequenceDur.readVideo(conn); // (课程视频名, 课程视频次序)
 
-		ResultSet rs = MySqlConn.getResultSet(conn, sql);
+		ResultSet rs = MySQLHelper.getResultSet(conn, sql);
 		long stuno = 0;
-		// int tlen = 0;
 		String title = "";
-		String sequence = "";
+		Integer video_sequence = 0;
+		HashMap<Integer, Integer> video_times = null;
 		try {
-			while(rs.next()) {
+			while (rs.next()) {
 				stuno = rs.getLong(1);
-				// tlen = rs.getInt(2);
-				title = rs.getString(3);
-				sequence = rs.getString(4);
-
-				int video_sequence = Integer.parseInt(sequence.split("\\.")[0]);
+				title = rs.getString(2);
 				
-				// calculate stuno_video_times
-				if(stuno_video_times.containsKey(stuno)) { // old student					
-					HashMap<Integer, Integer> video_times = stuno_video_times.get(stuno);					
-					if(video_times.containsKey(video_sequence)) { // watched video	
-						video_times.put(video_sequence, video_times.get(video_sequence) + 1);
-					} else { // new video
+				video_sequence = videos.get(title);
+				if (video_sequence != null) {
+					if (stuno_video_times.containsKey(stuno)) { // old student
+						video_times = stuno_video_times.get(stuno);				
+						if (video_times.containsKey(video_sequence)) { // watched video
+							video_times.put(video_sequence, video_times.get(video_sequence) + 1);
+						} else { // new video
+							video_times.put(video_sequence, 1);
+						}
+					} else { // new student with new video
+						video_times = new HashMap<Integer, Integer>();
 						video_times.put(video_sequence, 1);
+						
+						stuno_video_times.put(stuno, video_times);
 					}
-				} else { // new student with new video
-					HashMap<Integer, Integer> video_times = new HashMap<Integer, Integer>();
-					video_times.put(video_sequence, 1);
-					
-					stuno_video_times.put(stuno, video_times);
 				}
-				
-				// calculate stuno_videos_times_sum
-				if(stuno_videos_times_sum.containsKey(stuno)) {
-					stuno_videos_times_sum.put(stuno, stuno_videos_times_sum.get(stuno) + 1);
-				} else {
-					stuno_videos_times_sum.put(stuno, 1);
-				}
-				
-				// calculate stuno_videos
-				if(stuno_videos.containsKey(stuno)) {
-					stuno_videos.get(stuno).add(title);
-				} else {
-					HashSet<String> videos = new HashSet<String>();
-					videos.add(title);
-					stuno_videos.put(stuno, videos);
-				}			
-			}
-			
-			// calculate stuno_videos_num
-			for(Entry<Long, HashSet<String>> entry :stuno_videos.entrySet()) {
-				stuno_videos_num.put(entry.getKey(), entry.getValue().size());
 			}
 			
 		} catch (SQLException e) {
@@ -104,63 +78,102 @@ public class PreferenceTimes implements Preference {
 			}
 		}
 		
-		// print number of students
-		System.out.println("We got " + stuno_video_times.size() + " students!");
+		System.out.println(stuno_video_times.size() + " 个学生");
 		
 		// store the result
-		this.storePreferenceTimes(stuno_video_times, stuno_videos_times_sum, stuno_videos_num, path1, path2);
+		this.storePreferenceTimes(stuno_video_times, path1, path2);
 	}
 	
-	/**
-	 * 
-	 * @param stuno_video_times
-	 * @param stuno_videos_times_sum
-	 * @param stuno_videos_num
-	 */
-	private void storePreferenceTimes(Map<Long, HashMap<Integer, Integer>> stuno_video_times, 
-			Map<Long, Integer> stuno_videos_times_sum, Map<Long, Integer> stuno_videos_num, 
-			String path1, String path2) {
+	public void calPreference(Connection conn, List<LearningLog> logs, String path1, String path2) {
+		Map<Long, HashMap<Integer, Integer>> stuno_video_times = new HashMap<Long, HashMap<Integer, Integer>>();
+		
+		HashMap<String, Integer> videos = VideoSequenceDur.readVideo(conn); // (课程视频名, 课程视频次序)
+
+		long stuno = 0;
+		String title = "";
+		Integer video_sequence = 0;
+		HashMap<Integer, Integer> video_times = null;
+		
+		for(LearningLog aLearningLog : logs) {
+			stuno = aLearningLog.getStuno();
+			title = aLearningLog.getTitle();
+			
+			video_sequence = videos.get(title);
+			if (video_sequence != null) {
+				if (stuno_video_times.containsKey(stuno)) { // old student
+					video_times = stuno_video_times.get(stuno);				
+					if (video_times.containsKey(video_sequence)) { // watched video
+						video_times.put(video_sequence, video_times.get(video_sequence) + 1);
+					} else { // new video
+						video_times.put(video_sequence, 1);
+					}
+				} else { // new student with new video
+					video_times = new HashMap<Integer, Integer>();
+					video_times.put(video_sequence, 1);
+					
+					stuno_video_times.put(stuno, video_times);
+				}
+			}
+		}
+		
+		System.out.println(stuno_video_times.size() + " 个学生");
+		
+		// store the result
+		this.storePreferenceTimes(stuno_video_times, path1, path2);
+	}
+	
+	private void storePreferenceTimes(Map<Long, HashMap<Integer, Integer>> stuno_video_times, String path1, String path2) {
 		
 		StringBuilder preference_detail = new StringBuilder();
 		StringBuilder preference = new StringBuilder();
 		
-		long stuno = 0;
-		int video_sequence = 0;
-		double video_times = 0; // double!
-		double videos_times_sum = 0; // double!
-		double videos_num = 0; // double!
+		int[] max_video_times = new int[224]; // 存放每个视频被单个学习者观看的最大次数
 		
-		double preferenceTimes = 0;
-		for(Entry<Long, HashMap<Integer, Integer>> entry : stuno_video_times.entrySet()) {
+		long stuno = 0;
+		for (Entry<Long, HashMap<Integer, Integer>> entry : stuno_video_times.entrySet()) {
 			stuno = entry.getKey();
-			HashMap<Integer, Integer> hs_video_times = entry.getValue();
+			HashMap<Integer, Integer> video_times = entry.getValue();
 			
-			for(Entry<Integer, Integer> entry2 : hs_video_times.entrySet()) {
-				video_sequence = entry2.getKey();
-				video_times = entry2.getValue();
+			int sequence = 0;
+			int times = 0;
+			for (Entry<Integer, Integer> entry2 : video_times.entrySet()) {
+				sequence = entry2.getKey();
+				times = entry2.getValue();
+				if (times > max_video_times[sequence - 1]) {
+					max_video_times[sequence - 1] = times;
+				}
+			}
+		}
+		
+		for (Entry<Long, HashMap<Integer, Integer>> entry : stuno_video_times.entrySet()) {
+			stuno = entry.getKey();
+			HashMap<Integer, Integer> video_times = entry.getValue();
+
+			int sequence = 0;
+			double times = 0.0;
+			double preferenceTimes = 0;
+			for (Entry<Integer, Integer> entry2 : video_times.entrySet()) {
+				sequence = entry2.getKey();
+				times = entry2.getValue();
 				
-				videos_times_sum = stuno_videos_times_sum.get(stuno);
-				videos_num = stuno_videos_num.get(stuno);
-				
-				preferenceTimes = video_times / (videos_times_sum / videos_num);
+				preferenceTimes = times / max_video_times[sequence - 1];
 				
 				preference_detail.append(stuno);
-				preference_detail.append("," + video_sequence);
-				preference_detail.append("," + video_times);
-				preference_detail.append("," + videos_times_sum);
-				preference_detail.append("," + videos_num);
+				preference_detail.append("," + sequence);
+				preference_detail.append("," + times);
+				preference_detail.append("," + max_video_times[sequence - 1]);
 				preference_detail.append("\n");
 				
 				preference.append(stuno);
-				preference.append("," + video_sequence);
+				preference.append("," + sequence);
 				preference.append("," + preferenceTimes);
 				preference.append("\n");
 			}
 		}
 		
 		// stroe into file
-		StoreStringIntoFile.storeString(preference_detail.toString(), path1);
-		StoreStringIntoFile.storeString(preference.toString(), path2);
+		StoreStringIntoFile.storeString(preference_detail.toString(), path1, true);
+		StoreStringIntoFile.storeString(preference.toString(), path2, true);
 	}
 	
 }
