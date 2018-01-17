0c05656c3d85c855b2ff2226594943972c946b28
wuke1993 <2297184250@qq.com>
2018-01-17 16:24:54 +0800

    新建包 org.recommender.cf.similarity.forum

--- src/org/recommender/cf/similarity/forum/ForumCorrelation.java
+++ src/org/recommender/cf/similarity/forum/ForumCorrelation.java
@@ -1,4 +1,4 @@
-package org.recommender.cf.similarity;
+package org.recommender.cf.similarity.forum;
 /**
 * @author : wuke
 * @date   : 20170611 01:00:14
@@ -6,7 +6,5 @@ package org.recommender.cf.similarity;
 * Description : 
 */
 public interface ForumCorrelation {
-
 	double calForumCorrelation(int stuno_sequence_x, int stuno_sequence_y);
-	
 }
