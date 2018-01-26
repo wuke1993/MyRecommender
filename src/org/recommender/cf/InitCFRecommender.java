5fc4d65ad3ed91f125e05a579122b0b991ccb67e
wuke1993 <2297184250@qq.com>
2018-01-26 11:22:30 +0800

    优化，将评分计算，相似学习者计算，推荐结果评价分开到单独方法

--- src/org/recommender/cf/InitCFRecommender.java
+++ src/org/recommender/cf/InitCFRecommender.java
@@ -14,7 +14,9 @@ import org.recommender.cf.preference.PreferencePauseDrag;
 import org.recommender.cf.preference.PreferenceDuration;
 import org.recommender.cf.preference.PreferenceTimes;
 import org.recommender.cf.preference.VideoSequenceDur;
+import org.recommender.cf.rec.GenRecommendations;
 import org.recommender.cf.similarity.CalSimilarityMatrix;
+import org.recommender.cf.similarity.supplement.CoursewareSimilarity;
 import org.recommender.data.LearningLog;
 import org.recommender.data.TrainingSetTestSet;
 import org.recommender.measuring.Measuring;
@@ -28,7 +30,9 @@ import org.recommender.utility.MySQLHelper;
 * Description : 
 */
 public class InitCFRecommender {
-
+	public static double[][] preferenceMatrix = null;
+	public static int[][] neighborsMatrix = null;
+	
 	public static void main(String[] args) {
 		Connection conn = MySQLHelper.getConn();
 		
@@ -41,10 +45,11 @@ public class InitCFRecommender {
         List<LearningLog> testSet = logsAll.get(4);
         
         /*InitCFRecommender.initPreference(conn, trainSet);*/
+        InitCFRecommender.initPreferenceNeighborsMatrix(conn);
         
-        for (int i = 20; i <= 20; i++) {
-        	for (int j = 10; j <= 10; j++) {
-        		InitCFRecommender.init(conn, i, j, testSet);
+        for (int i = 2; i <= 10; i++) {
+        	for (int j = 1; j <= 5; j++) {
+        		InitCFRecommender.measureRec(conn, i, j, testSet);
         		System.out.println("***********");
         	}
         	System.out.println("==========================");
@@ -52,8 +57,7 @@ public class InitCFRecommender {
 	}
 	
 	/**
-	 * 第一步 评分生成
-	 * 此处注意打印出的三个学生数量，取最大值，手动赋给 USER_NUM TODO 需改进
+	 * 评分生成。注意：打印出的三个学生数量，取最大值，手动赋给 USER_NUM TODO 需改进
 	 * @param conn
 	 * @param trainSet
 	 */
@@ -76,44 +80,52 @@ public class InitCFRecommender {
 		CalPreference.calPreference(coefficient_times, coefficient_pause_drag, coefficient_duration, path2, path4, path6, preference_path);
 	}
 	
-	public static void init(Connection conn, int neighbors_num, int recommendation_num, List<LearningLog> testSet) {		
-		// 第二步，计算相似度，首先生成评分矩阵
+	/**
+	 * 评分矩阵 & 邻近用户矩阵生成
+	 * @param conn
+	 */
+	public static void initPreferenceNeighborsMatrix(Connection conn) {		
+		// 生成评分矩阵
 		int user_num = Integer.parseInt(PropertyHelper.getProperty("USER_NUM"));
 		int item_num = Integer.parseInt(PropertyHelper.getProperty("ITEM_NUM"));
 		
 		String preference_path = PropertyHelper.getProperty("PREFERENCE_PATH");
-		double[][] preferenceMatrix = GenPreferenceMatrix.genPreferenceMatrix(preference_path, user_num, item_num);
+		InitCFRecommender.preferenceMatrix = GenPreferenceMatrix.genPreferenceMatrix(preference_path, user_num, item_num);
 		
-		double[][] similarityMatrix = CalSimilarityMatrix.calSimilarityMatrix(preferenceMatrix, user_num);
+		// 计算相似度矩阵
+		HashMap<Integer, Integer> stuno_coursewareTimes = CoursewareSimilarity.genCoursewareTimes(conn);
+		double[][] similarityMatrix = CalSimilarityMatrix.calSimilarityMatrix(InitCFRecommender.preferenceMatrix, user_num, stuno_coursewareTimes);
+		// double[][] similarityMatrix = CalSimilarityMatrix.calSimilarityMatrix(preferenceMatrix, user_num);
 		
-		// 第三步，生成邻近用户
-		// int neighbors_num = Integer.parseInt(PropertyHelper.getProperty("PARAMETER_K"));
-		int[][] neighborsMatrix = CalNeighbors.calKNeighbors(user_num, neighbors_num, similarityMatrix);
+		// 生成邻近用户
+		int neighbors_num = Integer.parseInt(PropertyHelper.getProperty("PARAMETER_K")); // 20个
+		InitCFRecommender.neighborsMatrix = CalNeighbors.calKNeighbors(user_num, neighbors_num, similarityMatrix);
+	}
+	
+	public static void measureRec(Connection conn, int neighbors_num, int recommendation_num, List<LearningLog> testSet) {
+		// 生成推荐列表	
+		ArrayList<HashMap<Integer, Double>> recArr = GenRecommendations.genRecommendationForAll(InitCFRecommender.preferenceMatrix, 
+				InitCFRecommender.neighborsMatrix, neighbors_num, recommendation_num);
 		
-		// 第四步，生成推荐列表
-		// int recommendation_num = Integer.parseInt(PropertyHelper.getProperty("PARAMETER_N"));
-		// int neighbors_num = Integer.parseInt(PropertyHelper.getProperty("PARAMETER_K"));
+		String rec_path = PropertyHelper.getProperty("REC_PATH");
+		GenRecommendations.storeRecommendations(recArr, rec_path);
 		
+		// 算法评价
+		HashMap<Long, Integer> stunos_sequences = TrainingSetTestSet.getStunoSequence(PropertyHelper.getProperty("PREFERENCE_PATH"));
+		HashMap<String, Integer> videos_sequences = VideoSequenceDur.readVideo(conn);
+		HashMap<Integer, HashSet<Integer>> testSet_stuno_videos = TrainingSetTestSet.selectTestSet(testSet, stunos_sequences, videos_sequences);
+		
+		Measuring.calAccuracyRate(recArr, testSet_stuno_videos);
+	}
+	
+	private static void test() {
 		// generate recommendations for one
 		/*int user_sequence = 1; // 从 1 开始
-		HashMap<Integer, Double> rec = GenRecommendations.genRecommendationForOne(preferenceMatrix, neighborsMatrix, user_sequence, neighbors_num, recommendation_num);
+		HashMap<Integer, Double> rec = GenRecommendations.genRecommendationForOne(preferenceMatrix, neighborsMatrix, 
+		    user_sequence, neighbors_num, recommendation_num);
 		
 		for(Entry<Integer, Double> entry : rec.entrySet()) {
 			System.out.println(entry.getKey() + " " + entry.getValue());
 		}*/
-		
-		// generate recommendations for all
-		ArrayList<HashMap<Integer, Double>> recArr = GenRecommendations.genRecommendationForAll(preferenceMatrix, neighborsMatrix, neighbors_num, recommendation_num);
-		
-		// store recommendations into file
-		String rec_path = PropertyHelper.getProperty("REC_PATH");
-		GenRecommendations.storeRecommendations(recArr, rec_path);
-		
-		// 第五步，算法评价
-		HashMap<Long, Integer> stunos_sequences = TrainingSetTestSet.getStunoSequence(preference_path);
-		HashMap<String, Integer> videos_sequences = VideoSequenceDur.readVideo(conn);
-		HashMap<Integer, HashSet<Integer>> testSet_stuno_videos = TrainingSetTestSet.processTestSet(testSet, stunos_sequences, videos_sequences);
-		
-		Measuring.calAccuracyRate(recArr, testSet_stuno_videos);
 	}
 }
