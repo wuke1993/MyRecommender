2cd0d793ea9009aa1b2d06dab81450cd20b08956
wuke1993 <2297184250@qq.com>
2018-01-26 11:11:05 +0800

    学习者学习某个视频的总次数/该视频被单个学习者学习的最大次数

--- src/org/recommender/cf/preference/PreferenceTimes.java
+++ src/org/recommender/cf/preference/PreferenceTimes.java
@@ -9,27 +9,17 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.recommender.data.LearningLog;
-import org.recommender.utility.PropertyHelper;
 import org.recommender.utility.MySQLHelper;
+import org.recommender.utility.PropertyHelper;
 import org.recommender.utility.StoreStringIntoFile;
 
 /**
 * @author : wuke
 * @date   : 20170608 16:55:08
 * Title   : PreferenceTimes
-* Description : (stuno, (video, times))
+* Description : 学习者学习某个视频的总次数/该视频被单个学习者学习的最大次数
 */
 public class PreferenceTimes {
-	/*public static void main(String[] args) {
-		Connection conn = MySQLHelper.getConn();
-		
-		String tableName = "my_cs_log_stulearns_4th";
-		String sql = "SELECT stuno, title FROM " + tableName + " WHERE platform = 2 AND oper = 76";
-		
-		String path1 = GetProperty.getPropertyByName("PREFERENCE_TIMES_DETAIL_PATH");
-		String path2 = GetProperty.getPropertyByName("PREFERENCE_TIMES_PATH");
-		PreferenceTimes.calPreference(conn, sql, path1, path2);
-	}*/
 	
 	public static void calPreference(Connection conn, String sql, String path1, String path2) {
 		Map<Long, HashMap<Integer, Integer>> stuno_video_times = new HashMap<Long, HashMap<Integer, Integer>>();
@@ -72,45 +62,6 @@ public class PreferenceTimes {
 				conn.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
-			}
-		}
-		
-		System.out.println(stuno_video_times.size() + " 个学生");
-		
-		PreferenceTimes.storePreferenceTimes(stuno_video_times, path1, path2);
-	}
-	
-	public static void calPreference(Connection conn, List<LearningLog> logs, String path1, String path2) {
-		Map<Long, HashMap<Integer, Integer>> stuno_video_times = new HashMap<Long, HashMap<Integer, Integer>>();
-		
-		HashMap<String, Integer> videos = VideoSequenceDur.readVideo(conn); // (课程视频名, 课程视频次序)
-
-		long stuno = 0;
-		String title = "";
-		Integer video_sequence = 0;
-		HashMap<Integer, Integer> video_times = null;
-		
-		for(LearningLog aLearningLog : logs) {
-			if (aLearningLog.getOper() == 76) {
-				stuno = aLearningLog.getStuno();
-				title = aLearningLog.getTitle();
-				
-				video_sequence = videos.get(title);
-				if (video_sequence != null) {
-					if (stuno_video_times.containsKey(stuno)) { // old student
-						video_times = stuno_video_times.get(stuno);				
-						if (video_times.containsKey(video_sequence)) { // watched video
-							video_times.put(video_sequence, video_times.get(video_sequence) + 1);
-						} else { // new video
-							video_times.put(video_sequence, 1);
-						}
-					} else { // new student with new video
-						video_times = new HashMap<Integer, Integer>();
-						video_times.put(video_sequence, 1);
-						
-						stuno_video_times.put(stuno, video_times);
-					}
-				}
 			}
 		}
 		
@@ -173,4 +124,56 @@ public class PreferenceTimes {
 		StoreStringIntoFile.storeString(preference.toString(), path2);
 	}
 	
+	public static void test() {
+		Connection conn = MySQLHelper.getConn();
+		
+		String tableName = "my_cs_log_stulearns_4th";
+		String sql = "SELECT stuno, title FROM " + tableName + " WHERE platform = 2 AND oper = 76";
+		
+		String path1 = PropertyHelper.getProperty("PREFERENCE_TIMES_DETAIL_PATH");
+		String path2 = PropertyHelper.getProperty("PREFERENCE_TIMES_PATH");
+		PreferenceTimes.calPreference(conn, sql, path1, path2);
+	}
+	
+	/**
+	 * 弃用。从数据库中读取日志数据
+	 */
+	public static void calPreference(Connection conn, List<LearningLog> logs, String path1, String path2) {
+		Map<Long, HashMap<Integer, Integer>> stuno_video_times = new HashMap<Long, HashMap<Integer, Integer>>();
+		
+		HashMap<String, Integer> videos = VideoSequenceDur.readVideo(conn); // (课程视频名, 课程视频次序)
+
+		long stuno = 0;
+		String title = "";
+		Integer video_sequence = 0;
+		HashMap<Integer, Integer> video_times = null;
+		
+		for(LearningLog aLearningLog : logs) {
+			if (aLearningLog.getOper() == 76) {
+				stuno = aLearningLog.getStuno();
+				title = aLearningLog.getTitle();
+				
+				video_sequence = videos.get(title);
+				if (video_sequence != null) {
+					if (stuno_video_times.containsKey(stuno)) { // old student
+						video_times = stuno_video_times.get(stuno);				
+						if (video_times.containsKey(video_sequence)) { // watched video
+							video_times.put(video_sequence, video_times.get(video_sequence) + 1);
+						} else { // new video
+							video_times.put(video_sequence, 1);
+						}
+					} else { // new student with new video
+						video_times = new HashMap<Integer, Integer>();
+						video_times.put(video_sequence, 1);
+						
+						stuno_video_times.put(stuno, video_times);
+					}
+				}
+			}
+		}
+		
+		System.out.println(stuno_video_times.size() + " 个学生");
+		
+		PreferenceTimes.storePreferenceTimes(stuno_video_times, path1, path2);
+	}
 }
