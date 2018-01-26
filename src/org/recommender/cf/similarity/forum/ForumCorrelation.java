ef06d3a7589b227b7ca7636c6d70ac6ee32316ec
wuke1993 <2297184250@qq.com>
2018-01-26 11:13:14 +0800

    学习者论坛关联，待实现

--- src/org/recommender/cf/similarity/forum/ForumCorrelation.java
+++ src/org/recommender/cf/similarity/forum/ForumCorrelation.java
@@ -1,10 +1,14 @@
 package org.recommender.cf.similarity.forum;
 /**
 * @author : wuke
-* @date   : 20170611 01:00:14
-* Title   : ForumCorrelation
-* Description : 
+* @date   : 20170611 01:02:15
+* Title   : ForumPostAnswerCorrelation
+* Description : TODO 未实现
 */
-public interface ForumCorrelation {
-	double calForumCorrelation(int stuno_sequence_x, int stuno_sequence_y);
+public class ForumCorrelation {
+	public static double calForumCorrelation(int stuno_sequence_x, int stuno_sequence_y) {
+		double forum_post_answer_correlation = 0.0;
+		
+		return forum_post_answer_correlation;
+	}
 }
