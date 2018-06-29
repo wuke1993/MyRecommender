8618466609ba76d4b4f120da766ae7d2f621b723
wuke1993 <2297184250@qq.com>
2018-06-29 20:25:29 +0800

    评分矩阵构建及学习者相似度计算的权重选择

--- src/org/recommender/cf/AdjustWeight.java
+++ src/org/recommender/cf/AdjustWeight.java
@@ -26,6 +26,7 @@ public class AdjustWeight {
         trainSet.addAll(logsAll.get(1));trainSet.addAll(logsAll.get(2));trainSet.addAll(logsAll.get(3));
         List<LearningLog> testSet = logsAll.get(4);
         
+        //AdjustWeight.adjustPreferenceWeight(conn, trainSet, testSet);
         AdjustWeight.adjustSimilarityWeight(conn, trainSet, testSet);
 	}
 	
@@ -34,38 +35,19 @@ public class AdjustWeight {
         double accuracy = 0.0;
         double recall = 0.0;
         
-        /*int i = 1;
-        int j = 1;
-        int l = 1;
-        CFRecommender.initPreference(conn, trainSet, i, j, l);
-		CFRecommender.initPreferenceNeighborsMatrix(conn);
-		for (int k = 3; k <= 9; k++) {
-			for (int n = 2; n <= 5; n++) {
-			    CFRecommender.genRecommendation(k, n);
-			
-				accuracy_recall = CFRecommender.measureRec(conn, testSet);
-				
-				accuracy += accuracy_recall[0];
-				recall += accuracy_recall[1];
-			}
-		}
-		System.out.println(i + "," + j + "," + l);
-		System.out.println(accuracy + "," + recall);*/
-        
+		// 三个占比共 10 份，包含66种情况
         for (int i = 0; i <= 10; i++) {
         	for (int j = 0; j <= (10 - i); j++) {
         		int l = 10 - i -j;
         		
         		if (0 <= l && l <= 10) {
-        			
         			accuracy = 0.0;
         			recall = 0.0;
         			
         			CFRecommender.initPreference(conn, trainSet, i, j, l);
-        			
         			CFRecommender.initPreferenceNeighborsMatrix(conn);
         			
-        			for (int k = 3; k <= 9; k++) {
+        			for (int k = 3; k <= 20; k++) {
         				for (int n = 2; n <= 5; n++) {
         					CFRecommender.genRecommendation(k, n);
         					
@@ -76,11 +58,26 @@ public class AdjustWeight {
         				}
         			}
         			
-        			System.out.println(i + "," + j + "," + l);
-        			System.out.println(accuracy + "," + recall);
+        			//System.out.println(i + "," + j + "," + l);
+        			System.out.println(accuracy / 72 + "," + recall / 72);
         		}
         	}
         }
+        
+        /*// 1:1:1
+        CFRecommender.initPreference(conn, trainSet, 1, 1, 1);
+		CFRecommender.initPreferenceNeighborsMatrix(conn);
+		for (int k = 3; k <= 20; k++) {
+			for (int n = 2; n <= 5; n++) {
+			    CFRecommender.genRecommendation(k, n);
+			
+				accuracy_recall = CFRecommender.measureRec(conn, testSet);
+				
+				accuracy += accuracy_recall[0];
+				recall += accuracy_recall[1];
+			}
+		}
+		System.out.println(accuracy / 72 + "," + recall / 72);*/
 	}
 	
 	public static void adjustSimilarityWeight(Connection conn, List<LearningLog> trainSet, List<LearningLog> testSet) {
@@ -88,19 +85,7 @@ public class AdjustWeight {
         double accuracy = 0.0;
         double recall = 0.0;
         
-		CFRecommender.initPreferenceNeighborsMatrix(conn, 0, 1);
-		for (int k = 3; k <= 20; k++) {
-			for (int n = 2; n <= 5; n++) {
-				CFRecommender.genRecommendation(k, n);
-				accuracy_recall = CFRecommender.measureRec(conn, testSet);
-				
-				accuracy += accuracy_recall[0];
-				recall += accuracy_recall[1];
-			}
-		}
-		System.out.println(1 + "," + 0);
-		System.out.println(accuracy + "," + recall);
-        
+        // 两个占比共 10 份，包含9种情况
         /*for (int i = 1; i < 10; i++) {
         	int j = 10 - i;
         	
@@ -119,8 +104,22 @@ public class AdjustWeight {
 				}
 			}
 			
-			System.out.println(i + "," + j);
-			System.out.println(accuracy + "," + recall);
+			//System.out.println(i + "," + j);
+			System.out.println(accuracy / 72 + "," + recall / 72);
         }*/
+        
+        // 取值 (1, 0) 和 (0, 1)
+        CFRecommender.initPreferenceNeighborsMatrix(conn, 100, 1); // 0.13911740808450124,0.16120759699370302
+ 		for (int k = 3; k <= 20; k++) {
+ 			for (int n = 2; n <= 5; n++) {
+ 				CFRecommender.genRecommendation(k, n);
+ 				accuracy_recall = CFRecommender.measureRec(conn, testSet);
+ 				
+ 				accuracy += accuracy_recall[0];
+ 				recall += accuracy_recall[1];
+ 			}
+ 		}
+ 		//System.out.println(1 + "," + 0);
+ 		System.out.println(accuracy / 72 + "," + recall / 72);
 	}
 }
