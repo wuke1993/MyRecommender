6dc4085327f447843fa4bd754c4beb6577d79f15
wuke1993 <2297184250@qq.com>
2018-06-29 20:25:59 +0800

    评分矩阵生成

--- src/org/recommender/cf/preference/GenPreferenceMatrix.java
+++ src/org/recommender/cf/preference/GenPreferenceMatrix.java
@@ -15,13 +15,13 @@ import org.recommender.utility.StoreStringIntoFile;
 */
 public class GenPreferenceMatrix {
 
-	/*public static void main(String[] args) {
-		String preference_path = GetProperty.getPropertyByName("PREFERENCE_PATH");
-		int user_num = Integer.parseInt(GetProperty.getPropertyByName("USER_NUM"));
-		int item_num = Integer.parseInt(GetProperty.getPropertyByName("ITEM_NUM"));
+	public static void main(String[] args) {
+		String preference_path = PropertyHelper.getProperty("PREFERENCE_PATH");
+		int user_num = Integer.parseInt(PropertyHelper.getProperty("USER_NUM"));
+		int item_num = Integer.parseInt(PropertyHelper.getProperty("ITEM_NUM"));
 		
 		double[][] preferenceMatrix = GenPreferenceMatrix.genPreferenceMatrix(preference_path, user_num, item_num);
-	}*/
+	}
 	
 	public static double[][] genPreferenceMatrix(String path, int user_num, int item_num) {		
 		double[][] preferenceMatrix = new double[user_num][item_num];
@@ -49,8 +49,89 @@ public class GenPreferenceMatrix {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		GenPreferenceMatrix.storePreferenceMatrix(preferenceMatrix, PropertyHelper.getProperty("PREFERENCE_MATRIX_PATH"));
+		
+		return preferenceMatrix;
+	}
+	
+	/**
+	 * 20180602
+	 * @param path
+	 * @param path2
+	 * @param user_num
+	 * @param item_num
+	 * @return
+	 */
+	public static double[][] genPreferenceMatrix(String path, String path2,int user_num, int item_num) {		
+		double[][] preferenceMatrix = new double[user_num][item_num];
+		
+		int stuno_sequence = 0;
+		// long stuno = 0.0;
+		int item_sequence = 0;
+		double preference = 0.0;
+		BufferedReader br = null;
+		try {
+			File file = new File(path);
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				String[] strs = line.split(",");
+				
+				stuno_sequence = Integer.parseInt(strs[0]); // 从 1 开始
+				// stuno = Long.parseDouble(strs[1]);
+				item_sequence = Integer.parseInt(strs[2]); // 从 1 开始
+				preference = Double.parseDouble(strs[3]);
+				
+				preferenceMatrix[stuno_sequence - 1][item_sequence - 1] = preference;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 第一次IF-CF推荐的列表，加入到“学习者已学”，修改评分矩阵
+		try {
+			File file = new File(path2);
+			FileReader fr = new FileReader(file);
+			br = new BufferedReader(fr);
+			String line = "";
+			int lineNum = 1;
+			int item1, item2, item3, item4, item5;
+			while ((line = br.readLine()) != null) {
+				String[] strs = line.split(",");
+				
+				item1 = Integer.parseInt(strs[0]); // 从 1 开始
+				item2 = Integer.parseInt(strs[1]);
+				item3 = Integer.parseInt(strs[2]);
+				item4 = Integer.parseInt(strs[3]);
+				item5 = Integer.parseInt(strs[4]);
+				
+				// 计算评分的取值
+				double sumP = 0.0;
+				int count = 0;
+				for (double p : preferenceMatrix[lineNum - 1]) {
+					if (Math.abs(p - 0) > 0.00000000001) {
+						sumP += p;
+						count++;
+					}
+				}
+				double temP = sumP / count;
+				
+				// 可以看是否已有评分，已有的话：1. 保留原始；2. 叠加；3. 赋新值 √
+				preferenceMatrix[lineNum - 1][item1 - 1] = temP;
+				preferenceMatrix[lineNum - 1][item2 - 1] = temP;
+				preferenceMatrix[lineNum - 1][item3 - 1] = temP;
+				preferenceMatrix[lineNum - 1][item4 - 1] = temP;
+				preferenceMatrix[lineNum - 1][item5 - 1] = temP;
+				
+				lineNum++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		GenPreferenceMatrix.storePreferenceMatrix(preferenceMatrix, PropertyHelper.getProperty("NEW_PREFERENCE_MATRIX_PATH"));
 		
 		return preferenceMatrix;
 	}
